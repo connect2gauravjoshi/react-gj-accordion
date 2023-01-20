33e9a2af1b80866e906bb2b2e0aff44441c56538
Gaurav <connect2gauravjoshi@gmail.com>
2023-01-20 17:11:21 +0530

    added expand all or a particular item

--- src/Accordion/Accordion.js
+++ src/Accordion/Accordion.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Children, cloneElement, isValidElement, useState } from 'react';
 import './Accordion.css';
 
-const Accordion = (props) => {
+const Accordion = ({ children, allowMultipleExpanded, preExpanded, expandAll }) => {
+    const getInitialItemExpandStatusList = () => {
+        let list = [];
+        for (let index = 0; index < children.length; index++) {
+            let flag = false;
+            if (expandAll) flag = true;
+            else if(preExpanded && Array.isArray(preExpanded)) flag = preExpanded.includes(index) ? true : false
+            list.push(flag)
+        }
+        return list;
+    }
+    const [itemExpandStatusList, setItemExpandStatusList] = useState(getInitialItemExpandStatusList());
+
+    const handleItemExpandStatusListChange = (index, status) => {
+        setItemExpandStatusList(itemExpandStatusList.map((x, i) => i === index ? status : allowMultipleExpanded ? x : false));
+    }
+
     return (
         <div className='gj-accordion'>
-            {props.children}
+            {Children.map(children, (child, index) => {
+                if (!isValidElement(child)) return null;
+                else return cloneElement(child, { isOpen: itemExpandStatusList[index], setIsOpen: handleItemExpandStatusListChange, itemIndex: index });
+            })
+            }
         </div>
     );
 }
